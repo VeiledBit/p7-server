7de210cd9e2e058409b73eda9bb928803536144b
VeiledBit <12989770+VeiledBit@users.noreply.github.com>
2023-09-21 09:10:35 +0200

    Add prod config

--- src/config/postgre.js
+++ src/config/postgre.js
@@ -1,5 +1,33 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.POSTGRE_URL);
+let sequelize;
+
+if (process.env.IS_PROD == "false") {
+  sequelize = new Sequelize(
+    process.env.POSTGRE_DB,
+    process.env.POSTGRE_USER,
+    process.env.POSTGRE_PASSWORD,
+    {
+      host: process.env.POSTGRE_URL,
+      dialect: "postgres",
+    }
+  );
+} else {
+  sequelize = new Sequelize(
+    process.env.POSTGRE_DB,
+    process.env.POSTGRE_USER,
+    process.env.POSTGRE_PASSWORD,
+    {
+      host: process.env.POSTGRE_URL,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }
+  );
+}
 
 module.exports = sequelize;
